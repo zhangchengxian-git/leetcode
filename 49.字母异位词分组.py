0ccade73bbcdbbff4691845d0e6f7c43ba15d035
zhangchengxian <18746418176@163.com>
2023-04-10 21:09:24 +0800

    hash

---
+++ 49.字母异位词分组.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=49 lang=python3
+#
+# [49] 字母异位词分组
+#
+
+# @lc code=start
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = collections.defaultdict(list)
+        for st in strs:
+            counts = [0] * 26
+            for ch in st:
+                counts[ord(ch) - ord("a")] += 1
+            # 需要将 list 转换成 tuple 才能进行哈希
+            mp[tuple(counts)].append(st)
+        
+        return list(mp.values())
+
+
+# @lc code=end
+
