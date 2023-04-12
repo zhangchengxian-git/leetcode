3c8f221d54b9ec7cb6e7c944c1de1c872d3a3ce9
zhangchengxian <18746418176@163.com>
2023-04-12 22:20:46 +0800

    0-m行，1-n列上推到0行1-n列，左推到0-m行0列，0列需要额外记录

---
+++ 73.矩阵置零.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=73 lang=python3
+#
+# [73] 矩阵置零
+#
+
+# @lc code=start
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m, n = len(matrix), len(matrix[0])
+        col0 = False
+        for i in range(m):
+            if matrix[i][0] == 0:
+                col0 = True
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = matrix[0][j] = 0
+        for i in range(m-1, -1, -1):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+            if col0:
+                matrix[i][0] = 0
+# @lc code=end
+
